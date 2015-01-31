03abf3d7553d0e8c16d150822fea35db1e603f01
Cheng Lou <chenglou92@gmail.com>
2015-01-30 18:13:32 -0800

    Add basic tests

---
+++ tests.js
@@ -0,0 +1,19 @@
+var assert = require("assert");
+var cx = require('./classSet');
+
+describe('cx', function() {
+  it('keeps object keys with truthy values', function() {
+    assert.equal(cx({
+      a: true,
+      b: false,
+      c: 0,
+      d: null,
+      e: undefined,
+      f: 1,
+    }), 'a     f');
+  });
+
+  it('joins arrays of class names, excluding null values', function() {
+    assert.equal(cx('a', 0, null, undefined, 'b'), 'a 0   b');
+  });
+});
